25e504df5eb8b574e2ec690a8223987a8c202b4b
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-19 20:36:33 -0400

    finished reverse with bug

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,46 +1,65 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
  * Made WAY better by kristofer 6/16/20
  */
 public class PhoneBook {
-
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+
+        this.phonebook = new LinkedHashMap<>();
+        this.phonebook.putAll(map);
     }
 
     public PhoneBook() {
-        this(null);
+        this(new HashMap<>());
     }
 
     public void add(String name, String phoneNumber) {
+        if(phonebook.containsKey(name)) {
+                phonebook.get(name).add(phoneNumber);
+            }else
+
+            phonebook.put(name, new ArrayList<>());
+
     }
 
     public void addAll(String name, String... phoneNumbers) {
-    }
+        if(phonebook.get(name)==null) {
+            phonebook.put(name, new ArrayList<>(Arrays.asList(phoneNumbers)));
+        }else phonebook.get(name).add(Arrays.toString(phoneNumbers));
+//phonebook.computeIfAbsent( name, k -> new ArrayList<>(Array.asList(phoneNumbers)));
+}
 
     public void remove(String name) {
+        phonebook.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return phonebook.containsKey(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return phonebook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
+        for (Map.Entry<String,List<String>>phonebook : phonebook.entrySet()) {
+            String name = phonebook.getKey();
+            List<String> phoneNumbers = phonebook.getValue();
+            for(String number : phoneNumbers) {
+                if (number == phoneNumber) {
+                    return name;
+                }
+            }
+
+        }
         return null;
     }
 
@@ -49,6 +68,6 @@ public class PhoneBook {
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return phonebook;
     }
 }
